491b81a5a89e18618c2f4ce6f95652862e4fd444
Beka <example@gmail.com>
2020-04-24 23:01:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+// let something;
+// console.log(something);
+
+
+// let peson = {
+//     name: 'John',
+//     age : 25,
+//     isMarried: false
+
+// };
+
+// console.log(peson.name);
+
+// let arr = ['palm.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[2]);
+
+
+// // let answer = confirm('Are you here?');
+// // console.log(answer);
+// let answer = +prompt('Are you here?', 'Yes');
+// console.log(answer);
+
+
+// console.log('arr' + ' -object');
+
+// console.log(4 + ' -object');
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr--);
+// console.log(decr++);
+
+// console.log(5%2);
+
+// console.log('2' === 2);
+
+// let person = {
+//     name: "John",
+//     age: 23,
+//     isMarried: false
+// };
+
+// console.log(person.name);
+
+
+// let arr = ['plug.pmg', 'orange.jpg', 'apple.bmp'];
+
+
+
+// console.log(arr[0]);
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = +prompt('Are you 18?', 'Yes');
+
+// console.log(answer);
+
+// console.log('arr' + ' -object');
+// console.log(4 + ' -object');
+
+
+// let incr = 10,
+//     decr = 10;
+    
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
